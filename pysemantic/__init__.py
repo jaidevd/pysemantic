31ace96757cefb4bb1c6cea687f80d1734c7dc30
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-03-20 15:50:07 +0530

    Reorganization

---
+++ pysemantic/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 jaidev <jaidev@schrodinger.local>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+from project import Project
+
+__all__ = ['Project']
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+NAME = "pysemantic"
+
+setup(
+    name=NAME,
+    version='0.0.1',
+    author='Jaidev Deshpande',
+    author_email='jaidev@dataculture.in',
+    packages=['pysemantic']
+)
